23f4a4fc410bdb220bb003a37466bd6ad015682a
Ramit Goolry <ramitgoolry@gmail.com>
2024-01-19 14:44:27 +0530

    misc

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -69,8 +69,8 @@ local options = {
   formatting = formatting_style,
 
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["k"] = cmp.mapping.select_prev_item(),
+    ["j"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
