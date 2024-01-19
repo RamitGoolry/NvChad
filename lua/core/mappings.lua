ab549b3ad87c69a6e44584040e46b46d9e89bb91
Ramit Goolry <ramitgoolry@gmail.com>
2024-01-19 12:26:31 +0530

    Some extra mappings: vsplit, hsplit, NvimTreeToggle moved to <leader>n

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -23,15 +23,15 @@ M.general = {
     ["<C-j>"] = { "<C-w>j", "Window down" },
     ["<C-k>"] = { "<C-w>k", "Window up" },
 
+    -- splitting windows
+    ["<leader>%"] = {"<cmd>vsplit<CR>", "Split Window Vertically"},
+    ["<leader>\""] = {"<cmd>split<CR>", "Split Window Horizontally"},
+
     -- save
     ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
 
     -- Copy all
     ["<C-c>"] = { "<cmd> %y+ <CR>", "Copy whole file" },
-
-    -- line numbers
-    ["<leader>n"] = { "<cmd> set nu! <CR>", "Toggle line number" },
-    ["<leader>rn"] = { "<cmd> set rnu! <CR>", "Toggle relative number" },
 
     -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
     -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
@@ -258,7 +258,7 @@ M.nvimtree = {
 
   n = {
     -- toggle
-    ["<C-n>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
+    ["<leader>n"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
 
     -- focus
     ["<leader>e"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvimtree" },
