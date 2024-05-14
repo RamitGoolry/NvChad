fa192301c962221ad588a1f6cae96e894199fac4
Ramit Goolry <ramit@wandb.com>
2024-05-14 11:49:22 -0700

    Inlay hints work on work laptop

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -7,155 +7,155 @@ local utils = require 'core.utils'
 -- export on_attach & capabilities for custom lspconfigs
 
 M.on_attach = function(client, bufnr)
-  utils.load_mappings('lspconfig', { buffer = bufnr })
+	utils.load_mappings('lspconfig', { buffer = bufnr })
 
-  if client.server_capabilities.signatureHelpProvider then
-    require('nvchad.signature').setup(client)
-  end
+	if client.server_capabilities.signatureHelpProvider then
+		require('nvchad.signature').setup(client)
+	end
 
-  if
-    not utils.load_config().ui.lsp_semantic_tokens
-    and client.supports_method 'textDocument/semanticTokens'
-  then
-    client.server_capabilities.semanticTokensProvider = nil
-  end
+	if
+		not utils.load_config().ui.lsp_semantic_tokens
+		and client.supports_method 'textDocument/semanticTokens'
+	then
+		client.server_capabilities.semanticTokensProvider = nil
+	end
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 M.capabilities.textDocument.completion.completionItem = {
-  documentationFormat = { 'markdown', 'plaintext' },
-  snippetSupport = true,
-  preselectSupport = true,
-  insertReplaceSupport = true,
-  labelDetailsSupport = true,
-  deprecatedSupport = true,
-  commitCharactersSupport = true,
-  tagSupport = { valueSet = { 1 } },
-  resolveSupport = {
-    properties = {
-      'documentation',
-      'detail',
-      'additionalTextEdits',
-    },
-  },
+	documentationFormat = { 'markdown', 'plaintext' },
+	snippetSupport = true,
+	preselectSupport = true,
+	insertReplaceSupport = true,
+	labelDetailsSupport = true,
+	deprecatedSupport = true,
+	commitCharactersSupport = true,
+	tagSupport = { valueSet = { 1 } },
+	resolveSupport = {
+		properties = {
+			'documentation',
+			'detail',
+			'additionalTextEdits',
+		},
+	},
 }
 
 local lspconfig = require 'lspconfig'
 
 lspconfig.lua_ls.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
 
-  settings = {
-    Lua = {
-      hint = {
-        enable = true,
-      },
-      diagnostics = {
-        globals = { 'vim' },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand '$VIMRUNTIME/lua'] = true,
-          [vim.fn.expand '$VIMRUNTIME/lua/vim/lsp'] = true,
-          [vim.fn.stdpath 'data' .. '/lazy/ui/nvchad_types'] = true,
-          [vim.fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy'] = true,
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			hint = {
+				enable = true,
+			},
+			diagnostics = {
+				globals = { 'vim' },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand '$VIMRUNTIME/lua'] = true,
+					[vim.fn.expand '$VIMRUNTIME/lua/vim/lsp'] = true,
+					[vim.fn.stdpath 'data' .. '/lazy/ui/nvchad_types'] = true,
+					[vim.fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy'] = true,
+				},
+				maxPreload = 100000,
+				preloadFileSize = 10000,
+			},
+		},
+	},
 }
 
 lspconfig.rust_analyzer.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-  settings = {
-    ['rust-analyzer'] = {
-      assist = {
-        importGranularity = 'module',
-        importPrefix = 'by_self',
-      },
-      cargo = {
-        loadOutDirsFromCheck = true,
-      },
-      procMacro = {
-        enable = true,
-      },
-    },
-  },
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
+	settings = {
+		['rust-analyzer'] = {
+			assist = {
+				importGranularity = 'module',
+				importPrefix = 'by_self',
+			},
+			cargo = {
+				loadOutDirsFromCheck = true,
+			},
+			procMacro = {
+				enable = true,
+			},
+		},
+	},
 }
 
 lspconfig.gopls.setup {
-  on_attach = function(client, bufnr)
-    M.on_attach(client, bufnr)
+	on_attach = function(client, bufnr)
+		M.on_attach(client, bufnr)
 
-    -- Enable Inlay Hints
-    vim.lsp.inlay_hint(bufnr, true)
-  end,
-  capabilities = M.capabilities,
-  settings = {
-    gopls = {
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-      analyses = {
-        unusedparams = true,
-        nilness = true,
-        unusedwrite = true,
-        useany = true,
-      },
-      experimentalPostfixCompletions = true,
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
+		-- Enable Inlay Hints
+		vim.lsp.inlay_hint.enable(true)
+	end,
+	capabilities = M.capabilities,
+	settings = {
+		gopls = {
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+			analyses = {
+				unusedparams = true,
+				nilness = true,
+				unusedwrite = true,
+				useany = true,
+			},
+			experimentalPostfixCompletions = true,
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
 }
 
 lspconfig.tsserver.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
 }
 
 lspconfig.pyright.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-      },
-    },
-  },
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
 }
 
 lspconfig.terraformls.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
 }
 
 lspconfig.yamlls.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
 }
 
 lspconfig.helm_ls.setup {
-  settings = {
-    ['helm-ls'] = {
-      yamlls = {
-        path = 'yaml-language-server',
-      },
-    },
-  },
+	settings = {
+		['helm-ls'] = {
+			yamlls = {
+				path = 'yaml-language-server',
+			},
+		},
+	},
 }
 
 return M
