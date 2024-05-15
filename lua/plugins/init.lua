ebd170ed40b38f8149b4b5f4082cee938db46096
Ramit Goolry <ramit@wandb.com>
2024-05-15 02:01:44 -0700

    Fix folding

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -363,19 +363,23 @@ local default_plugins = {
   -- UFO: Code Folding
   {
     'kevinhwang91/nvim-ufo',
-    keys = { 'za', 'zm', 'zM', 'zr', 'zR' },
+    -- keys = { 'za', 'zm', 'zM', 'zr', 'zR' },
     dependencies = {
       'kevinhwang91/promise-async',
       'neovim/nvim-lspconfig',
     },
-    -- lazy = false,
+    lazy = false,
     config = function()
       vim.o.foldcolumn = '1' -- '0' is not bad
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
 
-      require('ufo').setup()
+      require('ufo').setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
+      }
     end,
   },
 
