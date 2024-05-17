610e419543077305466388942d228ba04d0ceb33
Ramit Goolry <ramit@wandb.com>
2024-05-16 21:26:41 -0700

    import snippet

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -7,120 +7,123 @@ local utils = require 'core.utils'
 -- export on_attach & capabilities for custom lspconfigs
 
 M.on_attach = function(client, bufnr)
-  utils.load_mappings('lspconfig', { buffer = bufnr })
+	utils.load_mappings('lspconfig', { buffer = bufnr })
 
-  if client.server_capabilities.signatureHelpProvider then
-    require('nvchad.signature').setup(client)
-  end
+	if client.server_capabilities.signatureHelpProvider then
+		require('nvchad.signature').setup(client)
+	end
 
-  if
-    not utils.load_config().ui.lsp_semantic_tokens
-    and client.supports_method 'textDocument/semanticTokens'
-  then
-    client.server_capabilities.semanticTokensProvider = nil
-  end
+	if
+		not utils.load_config().ui.lsp_semantic_tokens
+		and client.supports_method 'textDocument/semanticTokens'
+	then
+		client.server_capabilities.semanticTokensProvider = nil
+	end
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 M.capabilities.textDocument.completion.completionItem = {
-  documentationFormat = { 'markdown', 'plaintext' },
-  snippetSupport = true,
-  preselectSupport = true,
-  insertReplaceSupport = true,
-  labelDetailsSupport = true,
-  deprecatedSupport = true,
-  commitCharactersSupport = true,
-  tagSupport = { valueSet = { 1 } },
-  resolveSupport = {
-    properties = {
-      'documentation',
-      'detail',
-      'additionalTextEdits',
-    },
-  },
+	documentationFormat = { 'markdown', 'plaintext' },
+	snippetSupport = true,
+	preselectSupport = true,
+	insertReplaceSupport = true,
+	labelDetailsSupport = true,
+	deprecatedSupport = true,
+	commitCharactersSupport = true,
+	tagSupport = { valueSet = { 1 } },
+	resolveSupport = {
+		properties = {
+			'documentation',
+			'detail',
+			'additionalTextEdits',
+		},
+	},
 }
 
 local lspconfig = require 'lspconfig'
 
 lspconfig.lua_ls.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+	on_attach = function(client, bufnr)
+		M.on_attach(client, bufnr)
+		vim.lsp.inlay_hint.enable(true)
+	end,
+	capabilities = M.capabilities,
 
-  settings = {
-    Lua = {
-      hint = {
-        enable = true,
-      },
-      diagnostics = {
-        globals = { 'vim' },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand '$VIMRUNTIME/lua'] = true,
-          [vim.fn.expand '$VIMRUNTIME/lua/vim/lsp'] = true,
-          [vim.fn.stdpath 'data' .. '/lazy/ui/nvchad_types'] = true,
-          [vim.fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy'] = true,
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			hint = {
+				enable = true,
+			},
+			diagnostics = {
+				globals = { 'vim' },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand '$VIMRUNTIME/lua'] = true,
+					[vim.fn.expand '$VIMRUNTIME/lua/vim/lsp'] = true,
+					[vim.fn.stdpath 'data' .. '/lazy/ui/nvchad_types'] = true,
+					[vim.fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy'] = true,
+				},
+				maxPreload = 100000,
+				preloadFileSize = 10000,
+			},
+		},
+	},
 }
 
 lspconfig.rust_analyzer.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-  settings = {
-    ['rust-analyzer'] = {
-      assist = {
-        importGranularity = 'module',
-        importPrefix = 'by_self',
-      },
-      cargo = {
-        loadOutDirsFromCheck = true,
-      },
-      procMacro = {
-        enable = true,
-      },
-    },
-  },
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
+	settings = {
+		['rust-analyzer'] = {
+			assist = {
+				importGranularity = 'module',
+				importPrefix = 'by_self',
+			},
+			cargo = {
+				loadOutDirsFromCheck = true,
+			},
+			procMacro = {
+				enable = true,
+			},
+		},
+	},
 }
 
 lspconfig.gopls.setup {
-  on_attach = function(client, bufnr)
-    M.on_attach(client, bufnr)
-    vim.lsp.inlay_hint.enable(true)
-  end,
-  capabilities = M.capabilities,
-  settings = {
-    gopls = {
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-      analyses = {
-        unusedparams = true,
-        nilness = true,
-        unusedwrite = true,
-        useany = true,
-      },
-      experimentalPostfixCompletions = true,
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
+	on_attach = function(client, bufnr)
+		M.on_attach(client, bufnr)
+		vim.lsp.inlay_hint.enable(true)
+	end,
+	capabilities = M.capabilities,
+	settings = {
+		gopls = {
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+			analyses = {
+				unusedparams = true,
+				nilness = true,
+				unusedwrite = true,
+				useany = true,
+			},
+			experimentalPostfixCompletions = true,
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
 }
 
 lspconfig.tsserver.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
 }
 
 -- lspconfig.pyright.setup {
@@ -137,76 +140,76 @@ lspconfig.tsserver.setup {
 -- }
 
 lspconfig.basedpyright.setup {
-  on_attach = function(client, bufnr)
-    M.on_attach(client, bufnr)
-    vim.lsp.inlay_hint.enable(true)
-  end,
+	on_attach = function(client, bufnr)
+		M.on_attach(client, bufnr)
+		vim.lsp.inlay_hint.enable(true)
+	end,
 
-  capabilities = M.capabilities,
-  settings = {
-    basedpyright = {
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-      analysis = {
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticSeverityOverrides = {
-          reportDeprecated = false,
-          reportAssignmentType = false,
-          reportAny = false,
-          reportMissingModuleSource = false,
-          reportMissingTypeArgument = false,
-          reportMissingParameterType = false,
-          reportUnknownVariableType = false,
-          reportPrivateLocalImportUsage = false,
-          reportPrivateUsage = false,
-          reportUnreachable = false,
-          reportUnknownArgumentType = false,
-          reportUnknownLambdaType = false,
-          reportUnknownMemberType = false,
-          reportUnknownParameterType = false,
-          reportAttributeAccessIssue = false,
-          reportIgnoreCommentWithoutRule = false,
-          reportUninitializedInstanceVariable = false,
-          reportUnusedCallResult = false,
-          reportImplicitOverride = false,
-          reportUntypedFunctionDecorator = false,
-          reportArgumentType = false,
-          reportImplicitStringConcatenation = false,
-          reportUnnecessaryTypeIgnoreComment = false,
-          reportUnnecessaryComparison = 'information',
-          reportUnnecessaryIsInstance = 'information',
-        },
-      },
-    },
-  },
+	capabilities = M.capabilities,
+	settings = {
+		basedpyright = {
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+			analysis = {
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+				diagnosticSeverityOverrides = {
+					reportDeprecated = false,
+					reportAssignmentType = false,
+					reportAny = false,
+					reportMissingModuleSource = false,
+					reportMissingTypeArgument = false,
+					reportMissingParameterType = false,
+					reportUnknownVariableType = false,
+					reportPrivateLocalImportUsage = false,
+					reportPrivateUsage = false,
+					reportUnreachable = false,
+					reportUnknownArgumentType = false,
+					reportUnknownLambdaType = false,
+					reportUnknownMemberType = false,
+					reportUnknownParameterType = false,
+					reportAttributeAccessIssue = false,
+					reportIgnoreCommentWithoutRule = false,
+					reportUninitializedInstanceVariable = false,
+					reportUnusedCallResult = false,
+					reportImplicitOverride = false,
+					reportUntypedFunctionDecorator = false,
+					reportArgumentType = false,
+					reportImplicitStringConcatenation = false,
+					reportUnnecessaryTypeIgnoreComment = false,
+					reportUnnecessaryComparison = 'information',
+					reportUnnecessaryIsInstance = 'information',
+				},
+			},
+		},
+	},
 }
 
 lspconfig.terraformls.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
 }
 
 lspconfig.yamlls.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
 }
 
 lspconfig.helm_ls.setup {
-  settings = {
-    ['helm-ls'] = {
-      yamlls = {
-        path = 'yaml-language-server',
-      },
-    },
-  },
+	settings = {
+		['helm-ls'] = {
+			yamlls = {
+				path = 'yaml-language-server',
+			},
+		},
+	},
 }
 
 return M
--- lua/plugins/configs/luasnip.lua
+++ lua/plugins/configs/luasnip.lua
@@ -1,32 +1,83 @@
+-- This is going to be the meat of the plugin.
+function load_custom_snippets(luasnip)
+  local snippet = luasnip.snippet
+  local insert = luasnip.insert_node
+  local choice = luasnip.choice_node
+  local dynamic = luasnip.dynamic_node
+  local func = luasnip.function_node
+  local repeated = require('luasnip.extras').rep
+  local format_args = require('luasnip.extras.fmt').fmta
+
+  -- Lua snippets
+  local function resolve_variable_name(import_name)
+    local name = import_name[1][1]
+    local parts = vim.split(name, '.', {
+      plain = true,
+      trimempty = true,
+    })
+    -- Remove any instances of "vim" or "nvim" from the variable name
+    parts = vim.tbl_filter(function(part)
+      return part:lower() ~= 'vim' and part:lower() ~= 'nvim'
+    end, parts)
+
+    local last_part = parts[#parts]
+    if last_part then
+      last_part = last_part:lower()
+
+      -- replace any non-alphanumeric characters with underscores and trim
+      last_part = last_part:gsub('[^%w]', '_')
+      last_part = last_part:gsub('^[_]*', '')
+      last_part = last_part:gsub('[_]*$', '')
+    end
+
+    return last_part or 'module'
+  end
+
+  luasnip.add_snippets('lua', {
+    snippet(
+      'require',
+      format_args('local <var> = require(\'<module>\')', {
+        var = func(resolve_variable_name, { 1 }),
+        module = insert(1),
+      })
+    ),
+  })
+end
+
 local exports = {}
 
 exports.load = function(opts)
-	local luasnip = require 'luasnip'
-	luasnip.config.set_config(opts)
+  local luasnip = require 'luasnip'
+  luasnip.config.set_config(opts)
 
-	-- vscode format
-	require('luasnip.loaders.from_vscode').lazy_load()
-	require('luasnip.loaders.from_vscode').lazy_load { paths = vim.g.vscode_snippets_path or '' }
+  -- vscode format
+  local luasnip_vscode_loader = require 'luasnip.loaders.from_vscode'
+  luasnip_vscode_loader.lazy_load()
+  luasnip_vscode_loader.lazy_load { paths = vim.g.vscode_snippets_path or '' }
 
-	-- snipmate format
-	require('luasnip.loaders.from_snipmate').load()
-	require('luasnip.loaders.from_snipmate').lazy_load { paths = vim.g.snipmate_snippets_path or '' }
+  -- snipmate format
+  local luasnip_snipmate_loader = require 'luasnip.loaders.from_snipmate'
+  luasnip_snipmate_loader.load()
+  luasnip_snipmate_loader.lazy_load { paths = vim.g.snipmate_snippets_path or '' }
 
-	-- lua format
-	require('luasnip.loaders.from_lua').load()
-	require('luasnip.loaders.from_lua').lazy_load { paths = vim.g.lua_snippets_path or '' }
+  -- lua format
+  local luasnip_lua_loader = require 'luasnip.loaders.from_lua'
+  luasnip_lua_loader.load()
+  luasnip_lua_loader.lazy_load { paths = vim.g.lua_snippets_path or '' }
 
-	vim.api.nvim_create_autocmd('InsertLeave', {
-		callback = function()
-			if
-				require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
-				and not require('luasnip').session.jump_active
-			then
-				require('luasnip').unlink_current()
-			end
-		end,
-	})
-	print 'Luasnip loaded!'
+  load_custom_snippets(luasnip)
+
+  vim.api.nvim_create_autocmd('InsertLeave', {
+    callback = function()
+      if
+        luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not luasnip.session.jump_active
+      then
+        luasnip.unlink_current()
+      end
+    end,
+  })
+  print 'Luasnip loaded!'
 end
 
 return exports
