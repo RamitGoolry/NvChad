450a858d26677ecd347ef9b51582277a2e0368df
Ramit Goolry <ramit@wandb.com>
2024-11-08 19:34:58 -0500

    Avante settings, move to left

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1345,9 +1345,9 @@ local default_plugins = {
         hints = { enabled = true },
         windows = {
           ---@type "right" | "left" | "top" | "bottom"
-          position = 'right', -- the position of the sidebar
+          position = 'left', -- the position of the sidebar
           wrap = true, -- similar to vim.o.wrap
-          width = 30, -- default % based on available width
+          width = 50, -- default % based on available width
           sidebar_header = {
             enabled = true, -- true, false to enable/disable the header
             align = 'center', -- left, center, right for title
