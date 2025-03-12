0323afee20445248e9448a0fa8d193eca0525fa7
Ramit Goolry <ramit@wandb.com>
2025-03-12 11:33:31 -0700

    MCP Hub: Mason, but for MCPs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1118,6 +1118,38 @@ local default_plugins = {
       remote_nvim.setup(remote_nvim_conf)
     end,
   },
+
+  -- MCP Hub: Mason, but for MCPs
+  {
+    'ravitemer/mcphub.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- Required for Job and HTTP requests
+    },
+    build = 'npm install -g mcp-hub@latest', -- Installs required mcp-hub npm module
+    config = function()
+      require('mcphub').setup {
+        -- Required options
+        port = 15000,
+        config = vim.fn.expand '~/.config/nvim/mcpservers.json',
+
+        -- Optional options
+        on_ready = function(hub)
+          -- Called when hub is ready
+        end,
+        on_error = function(err)
+          -- Called on errors
+        end,
+        shutdown_delay = 0, -- Wait 0ms before shutting down server after last client exits
+        log = {
+          level = vim.log.levels.WARN,
+          to_file = false,
+          file_path = nil,
+          prefix = 'MCPHub',
+        },
+      }
+    end,
+  },
 }
 
 local config = require('core.utils').load_config()
